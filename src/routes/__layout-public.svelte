4cf47c9c596e165733a2376f97bdece494e08679
Travis Russi <github@russigroup.com>
2022-07-29 11:53:18 -0400

    initial commit with basic routes

---
+++ src/routes/__layout-public.svelte
@@ -0,0 +1,12 @@
+<nav>
+	<a href="/">Home</a>
+	<a href="/product-detail">Product Detail</a>
+	<a href="/cart">Cart</a>
+	<a href="/checkout">Checkout</a>
+
+	<a href="/admin">Admin</a>
+</nav>
+
+<h1>Public Layout</h1>
+
+<slot />
